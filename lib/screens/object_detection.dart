9833ef4bc5f6bb1dac77e759f7d3759f8ab1138d
ImKavinduSandaruwan <githubsandaruwan651@gmail.com>
2024-01-26 05:06:33 +0530

    Added Camera part to the application

--- lib/screens/object_detection.dart
+++ lib/screens/object_detection.dart
@@ -1,6 +1,7 @@
 import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
 import '../constants/constant.dart';
+import 'package:camera/camera.dart';
 
 class ObjectDetection extends StatefulWidget {
   const ObjectDetection({super.key});
@@ -10,8 +11,43 @@ class ObjectDetection extends StatefulWidget {
 }
 
 class _ObjectDetectionState extends State<ObjectDetection> {
+
+  late CameraController controller;
+  late List<CameraDescription> _cameras;
+
+  Future<void> accessCamera() async {
+    WidgetsFlutterBinding.ensureInitialized();
+    _cameras = await availableCameras();
+    controller = CameraController(_cameras[0], ResolutionPreset.high);
+    controller.initialize().then((_) {
+      if (!mounted) {
+        return;
+      }
+      setState(() {});
+    }).catchError((Object e) {
+      if (e is CameraException) {
+        switch (e.code) {
+          case 'CameraAccessDenied':
+          // Handle access errors here.
+            break;
+          default:
+          // Handle other errors here.
+            break;
+        }
+      }
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    accessCamera();
+  }
+
   @override
   Widget build(BuildContext context) {
+    final double screenHeight = MediaQuery.of(context).size.height;
+    final double screenWidth = MediaQuery.of(context).size.width;
     return Scaffold(
       backgroundColor: const Color(0xffF2FEFE),
       bottomNavigationBar: CurvedNavigationBar(
@@ -22,6 +58,7 @@ class _ObjectDetectionState extends State<ObjectDetection> {
         backgroundColor: Colors.white,
         height: 60,
       ),
+      /// App is optional.🤔🤔
       appBar: AppBar(
         backgroundColor: mainThemeColor,
         title: const Text(
@@ -31,6 +68,19 @@ class _ObjectDetectionState extends State<ObjectDetection> {
             fontFamily: 'Poppins'
           ),
         ),
+      ),
+      body: Container(
+        margin: const EdgeInsets.symmetric(vertical: 10,horizontal: 10),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(20),
+            border: Border.all(
+                color: const Color(0xffC5C5C5),
+                width: 2
+            )
+        ),
+        width: screenWidth/2,
+        height: screenHeight/3,
+        child: CameraPreview(controller),
       ),
     );
   }
