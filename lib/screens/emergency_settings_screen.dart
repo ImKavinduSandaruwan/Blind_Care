11cd6dc06d8209f32d6551b45371a7295da067aa
Ginura Premawardana <ginujj9@gmail.com>
2024-02-16 10:16:21 +0530

    Rearrange the functionality of adding emergency contacts

--- lib/screens/emergency_settings_screen.dart
+++ lib/screens/emergency_settings_screen.dart
@@ -1,13 +1,8 @@
 
-import 'package:fast_contacts/fast_contacts.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_phone_direct_caller/flutter_phone_direct_caller.dart';
 import 'package:curved_navigation_bar/curved_navigation_bar.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:permission_handler/permission_handler.dart';
 import 'package:projectblindcare/constants/constant.dart';
-import 'package:geolocator/geolocator.dart';
 import 'package:projectblindcare/screens/emergency_screen.dart';
 import 'package:projectblindcare/screens/home_screen.dart';
 
@@ -20,54 +15,128 @@ class EmergencySettingsScreen extends StatefulWidget {
 }
 
 
-Map<String, String> emgContacts = {};
-List<bool> conAddedList = List.filled(1000, true);
-
 
 class _EmergencySettingsScreenState extends State<EmergencySettingsScreen> {
 
-  List<Widget> conIcon = [
-    Icon(
-      Icons.add,
-      color: Colors.green,
-    ),
-    Icon(
-      Icons.emergency,
-      color: Colors.red,
-    )
-  ];
+  List<Widget> addedContacts = [];
 
-  bool conAdded = true;
+  TextEditingController nameController = TextEditingController();
+  TextEditingController phoneNumberController = TextEditingController();
+
+
+  void _showDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Dialog Title'),
+          content: Container(
+            constraints: BoxConstraints(maxHeight: 200),
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  TextField(
+                    controller: nameController,
+                    decoration: InputDecoration(hintText: 'Enter Text 1'),
+                  ),
+                  SizedBox(height: 10),
+                  TextField(
+                    controller: phoneNumberController,
+                    decoration: InputDecoration(hintText: 'Enter Text 2'),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                EmergencyCantactListHandler.addDynamicWidget(nameController.text, phoneNumberController.text);
+                _addedSavedContact(nameController.text,phoneNumberController.text);
+                nameController.clear();
+                phoneNumberController.clear();
+                Navigator.of(context).pop();
+                setState(() {
+                  addedContacts;
+                });
+              },
+              child: const Text('Done'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _addedSavedContact(String name, String phone){
+    addedContacts.add(
+        Container(
+          margin: EdgeInsets.only(top: 10.0),
+          decoration: BoxDecoration(
+            border: Border.all(
+              color: mainThemeColor, // Set the color of the border
+              width: 2.0,           // Set the width of the border
+            ),
+            borderRadius: BorderRadius.all(Radius.circular(10.0)),  // Set border radius if you want rounded corners
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children:<Widget> [
+
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(left: 8.0,top: 0,right: 8.0,bottom: 0),
+                          child: Icon(Icons.account_circle),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(left: 8.0,top: 0,right: 8.0,bottom: 0),
+                          child: Text(name),
+                        ),
+                      ],
+                    ),
+                    Divider(),
+                    Row(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(left: 8.0,top: 0,right: 8.0,bottom: 0),
+                          child: Icon(Icons.phone),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(left: 8.0,top: 0,right: 8.0,bottom: 0),
+                          child: Text(phone),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+                IconButton(
+                    onPressed: (){},
+                    icon: Icon(Icons.delete)
+                )
+              ],
+            ),
+          ),
+        )
+    );
+  }
 
 
 
   @override
   Widget build(BuildContext context) {
-
-    Widget statusIcon;
-
-    if(conAdded == true){
-      statusIcon = conIcon.first;
-    }else{
-      statusIcon = conIcon.last;
-    }
-
-    for(int i=0;i<conAddedList.length;i++){
-
-    }
-
     double screenWidth = MediaQuery.of(context).size.width;
     double screenHeight = MediaQuery.of(context).size.height;
-
-
-
 
     return Scaffold(
       backgroundColor: const Color(0xffF2FEFE),
       bottomNavigationBar: CurvedNavigationBar(
-        items: const [
-          Icon(Icons.keyboard_voice_rounded),
-        ],
+        items: const [Icon(Icons.keyboard_voice_rounded)],
         color: mainThemeColor,
         backgroundColor: Colors.transparent,
         height: 60,
@@ -76,10 +145,7 @@ class _EmergencySettingsScreenState extends State<EmergencySettingsScreen> {
         backgroundColor: mainThemeColor,
         title: const Text(
           "Emergency Settings",
-          style: TextStyle(
-            color: Colors.black,
-            fontFamily: 'Poppins',
-          ),
+          style: TextStyle(color: Colors.black, fontFamily: 'Poppins'),
         ),
         leading: IconButton(
           icon: Icon(Icons.arrow_back),
@@ -88,143 +154,37 @@ class _EmergencySettingsScreenState extends State<EmergencySettingsScreen> {
           },
         ),
       ),
-      body: FutureBuilder(
-        future: getContacts(),
-        builder: (context, AsyncSnapshot snapshot) {
-          if (snapshot.data == null) {
-            return const Center(
-              child: SizedBox(height: 50, child: CircularProgressIndicator()),
-            );
-          }
-          return Container(
-            width: screenWidth, // Set appropriate width
-            height: screenHeight, // Set appropriate height
-            child: ListView.builder(
-              itemCount: snapshot.data.length,
-              itemBuilder: (context, index) {
-                Contact contact = snapshot.data[index];
-                bool conAdded = conAddedList[index];
-                return Padding(
-                  padding: const EdgeInsets.all(12.0),
-                  child: Container(
-                    decoration: BoxDecoration(
-                        border: Border.all(color: Colors.green),
-                        borderRadius: BorderRadius.circular(10)
-                    ),
-                    child: ListTile(
-                      title: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children:<Widget> [
-
-                            Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Row(
-                                  children: [
-                                    Padding(
-                                      padding: const EdgeInsets.only(left: 8.0,top: 0,right: 8.0,bottom: 0),
-                                      child: Icon(Icons.account_circle),
-                                    ),
-                                    Padding(
-                                      padding: const EdgeInsets.only(left: 8.0,top: 0,right: 8.0,bottom: 0),
-                                      child: Text(contact.displayName),
-                                    ),
-                                  ],
-                                ),
-                                Divider(),
-                                Row(
-                                  children: [
-                                    Padding(
-                                      padding: const EdgeInsets.only(left: 8.0,top: 0,right: 8.0,bottom: 0),
-                                      child: Icon(Icons.phone),
-                                    ),
-                                    Padding(
-                                      padding: const EdgeInsets.only(left: 8.0,top: 0,right: 8.0,bottom: 0),
-                                      child: Text(contact.phones[0].number),
-                                    ),
-                                  ],
-                                )
-                              ],
-                            ),
-                            IconButton(
-                              onPressed: () {
-                                EmergencyCantactListHandler.addDynamicWidget(contact.displayName, contact.phones[0].number);
-                                ContactDataModel newContact = ContactDataModel(name: '${contact.displayName}', phoneNumber: '${contact.phones[0].number}');
-                                EmergencyCantactListHandler.addContact(newContact);
-
-                                setState(() {
-                                  conAddedList[index] = false;
-                                });
-
-                                emgContacts[contact.displayName] = contact.phones[0].number;
-                                // print(index);
-
-                              },
-                              icon: conAdded ? conIcon.first : conIcon.last,
-                              color: Colors.red,
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                  ),
-                );
-              },
-            ),
-          );
-        },
-      ),
-    );
-}
-
-  Future openDialogBox(BuildContext context) => showDialog(
-      context: context,
-      builder: (context) => AlertDialog(
-        title: Text("Contact 1"),
-        content: Container(
-          height: 100, // Set the desired height
+      body: Container(
+        width: screenWidth,
+        height: screenHeight,
+        child: SingleChildScrollView(
           child: Column(
             children: [
-              TextField(
-                decoration: InputDecoration(hintText: 'Ginura'),
+              Padding(
+                  padding: const EdgeInsets.all(12.0),
+                  child: Column(
+                    children: addedContacts,
+                  )
               ),
-              TextField(
-                decoration: InputDecoration(hintText: 'Phone Number'),
-                keyboardType: TextInputType.phone,
-                inputFormatters: [
-                  LengthLimitingTextInputFormatter(13), // +94 and 10 digits
-                ],
-                onSubmitted: (_) => Navigator.of(context).pop(),
-              ),
+
+              Container(
+                margin: EdgeInsets.only(top: 20.0, bottom: 20.0),
+                height: 60.0,
+                width: screenWidth * 0.9,
+                child: ElevatedButton(
+                  onPressed: () {
+                    _showDialog(context);
+                  },
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all<Color>(Color.fromRGBO(153, 255, 153, 1.0)),
+                  ),
+                  child: Text("ADD"),
+                ),
+              )
             ],
           ),
         ),
-        actions: [
-          TextButton(
-              onPressed: (){
-                Navigator.of(context).pop();
-              },
-              child: Text("Done")
-          )
-        ],
-      )
-  );
-
-  Future<List<Contact>> getContacts() async {
-
-    bool isGranted = await Permission.contacts.status.isGranted;
-
-    if(!isGranted){
-      isGranted = await Permission.contacts.request().isGranted;
-    }
-
-    if(isGranted){
-      return await FastContacts.getAllContacts();
-    }
-    return [];
+      ),
+    );
   }
-
 }
-
