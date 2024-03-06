ec3c2cf52a9c38e968413e2f76bb9a27406a83d7
Infas23 <iqbal.20220923@iit.ac.lk>
2024-03-07 00:00:55 +0530

    Main Voice Functionality Complete

--- lib/screens/navigation_maps.dart
+++ lib/screens/navigation_maps.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:curved_navigation_bar/curved_navigation_bar.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get/get_core/src/get_main.dart';
@@ -17,6 +18,13 @@ import 'package:url_launcher/url_launcher.dart';
 import 'package:projectblindcare/components/camera_view.dart';
 import '../components/scan_controller.dart';
 import '../constants/constant.dart';
+
+//import 'package:alan_voice/alan_voice.dart';
+
+import 'package:speech_to_text/speech_to_text.dart';
+import 'package:speech_to_text/speech_recognition_result.dart';
+
+import 'package:flutter_tts/flutter_tts.dart';
 
 
 void main(){
@@ -39,64 +47,268 @@ class LocationMap extends StatefulWidget {
 }
 
 class _LocationMapState extends State<LocationMap> {
+  _LocationMapState(){
+    /*AlanVoice.addButton("689e5df914105717e09b84ae8ac4018d2e956eca572e1d8b807a3e2338fdd0dc/stage");
+
+    AlanVoice.playCommand("active now");
+
+    AlanVoice.onCommand.add((command) {
+      debugPrint("got new command ${command.toString()}");
+    });
+    AlanVoice.callbacks.add((command) {
+      _handleCommand(command.data);
+    });*/
+    _flutterTts.setLanguage("en-US"); // Set English (US) as default language
+    _flutterTts.setPitch(1.0); // Adjust pitch as needed (1.0 is default)
+    _flutterTts.setSpeechRate(0.5);
+  }
+  //bool alanRuning = AlanVoice.isActive() as bool;
+
+  /*void runAlan(){
+    AlanVoice.addButton("689e5df914105717e09b84ae8ac4018d2e956eca572e1d8b807a3e2338fdd0dc/stage");
+    AlanVoice.playCommand("active now");
+    AlanVoice.playCommand("command");
+
+    AlanVoice.onCommand.add((command) {
+      debugPrint("got new command ${command.toString()}");
+    });
+  }*/
+
+  void _handleCommand(Map<String, dynamic> command) {
+    switch(command["command"]){
+      case "start":
+        ;
+        break;
+      case "decrement":
+        ;
+        break;
+      default:
+        debugPrint("Unknown Command");
+    }
+  }
+
+  @override
+  void initState(){
+    super.initState();
+    _requestLocationPermission();
+
+    setState(() => _isListening = false);
+
+    _initSpeech();
+    _flutterTts.speak("Welcome to Navigation");
+
+    apiKey = 'AIzaSyBhwULmCSuNyr7hpqt-u9zEHydv31ucMfo';
+    googlePlace = GooglePlace(apiKey);
+    endFocusNode = FocusNode();
+
+
+    _navReady = false;
+  }
+
+  final FlutterTts _flutterTts = FlutterTts();
+
+
+  late TextEditingController _numberController = TextEditingController();
+
+  bool _navReady = false;
+
+  SpeechToText _speechToText = SpeechToText();
+  bool _isListening = false;
+  bool _isSpeaking = false;
+  String _lastWords = '';
+  Duration? listenDelay = Duration(seconds: 3);
+
+  void _initSpeech() async {
+    await Future.delayed(listenDelay!);
+    _requestMicrophoneAcess();
+    _speechToText.initialize();
+    _speechToText.listen(
+      listenFor: listenDelay,
+    );
+    setState(() {});
+  }
+  _startListening() async {
+    try {
+      print("_startListening");
+      print(_isListening);
+      print(_isSpeaking);
+      if (_isListening && !_isSpeaking) {
+        await _speechToText.listen(onResult: _onSpeechResult);
+        await Future.delayed(Duration(seconds: 5));
+        _isListening = false;
+        print(_isListening);
+      }
+      setState(() {
+        _lastWords = '';
+      });
+      print("_startListening");
+    }
+    catch (e){
+      _isListening = false;
+      print(e);
+    }
+  }
+  void _stopListening() async {
+    if (_isListening) {
+      _isListening = false;
+      await _speechToText.stop();
+    }
+    setState(() {});
+  }
+
+  _speak(String Text) async {
+    print("_speak");
+    if (!_isListening){
+      setState(() => _isSpeaking = true);
+      print(_isSpeaking);
+      await _flutterTts.setLanguage('en-US');
+      await _flutterTts.speak(Text);
+      setState(() => _isSpeaking = false);
+      print(_isSpeaking);
+      print("_speak");
+    }
+  }
+
+  void _onSpeechResult(SpeechRecognitionResult result) {
+    print("_onSpeechResult");
+    print(_isListening);
+    print(_isSpeaking);
+    if (_isSpeaking){
+      return null;
+    }
+    setState(() {
+      _lastWords = result.recognizedWords;
+      _destinationController.text = _lastWords;
+      _flutterTts.speak(_lastWords);
+      print(_lastWords);
+    });
+    autoCompleteSearch(_lastWords);
+    print("_onSpeechResult");
+  }
+
+  int? _getNumValue(){
+    try {
+      return int.parse(_numberController.text)-1;
+    } catch (e){
+      return null;
+    }
+  }
+
+  String _numValue = '';
+  _numListener() async {
+    print("_numListener");
+    _numValue = '';
+    print(_isListening);
+    print(_isSpeaking);
+    if (_isListening && !_isSpeaking) {
+      await _speechToText.listen(onResult: _onNumResult);
+      await Future.delayed(Duration(seconds: 5));
+      _isListening = false;
+      print(_isListening);
+    }
+    setState(() {
+      _numValue = '';
+    });
+    print("_numListener");
+  }
+  void _onNumResult(SpeechRecognitionResult numResult){
+    print("_onNumResult");
+    print(_isListening);
+    print(_isSpeaking);
+    if (_isSpeaking){
+      return null;
+    }
+    setState(() {
+      _numValue = numResult.recognizedWords;
+      _numberController.text = _numValue;
+      _flutterTts.speak(_numValue);
+      print(_numValue);
+    });
+    print("_onNumResult");
+  }
+
+  _enterDestination() async{
+    _requestMicrophoneAcess();
+    _navReady = false;
+    print("_enterDestination");
+    print(_isListening);
+    print(_isSpeaking);
+    if (_isListening || _isSpeaking){
+      return null;
+    }
+    _lastWords = '';
+    print(_lastWords);
+    _destinationController.clear();
+    predictions.clear();
+    _isSpeaking = true;
+    await _speak("Where do you want to go?");
+    print("POINT 1");
+    _flutterTts.awaitSpeakCompletion(true);
+    //AlanVoice.deactivate();
+    _isListening = true;
+    await Future.delayed(const Duration(seconds: 1));
+    print("POINT 2");
+    await _startListening();
+    _isListening = false;
+    print("_enterDestination");
+  }
+
   GoogleMapController? mapController;
   final TextEditingController _destinationController = TextEditingController();
   Set<Marker> markers = {};
 
-  Position? currentPosition;
+  final int delayInMilliseconds = 2000;
 
+  Position? currentPosition;
   late GooglePlace googlePlace;
   List<AutocompletePrediction> predictions = [];
   DetailsResult? endPosition;
   late FocusNode endFocusNode;
-
   double? s1,s2,e1,e2;
-
   Map<PolylineId, Polyline> polylines = {};
   List<LatLng> polylineCoordinates = [];
   PolylinePoints polylinePoints = PolylinePoints();
   String apiKey = "AIzaSyBhwULmCSuNyr7hpqt-u9zEHydv31ucMfo";
 
-  @override
-  void initState() {
-    super.initState();
-    apiKey = 'AIzaSyBhwULmCSuNyr7hpqt-u9zEHydv31ucMfo';
-    googlePlace = GooglePlace(apiKey);
 
-    _requestLocationPermission();
-
-    endFocusNode = FocusNode();
-  }
 
   @override
   void dispose() {
     // TODO: implement dispose
     super.dispose();
+    _flutterTts.stop();
+    _speechToText.stop();
     endFocusNode.dispose();
   }
+
+  voiceMain() async {
+    await _enterDestination();
+    await Future.delayed(const Duration(seconds: 1));
+
+}
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: const Color(0xffF2FEFE),
-        bottomNavigationBar: CurvedNavigationBar(
-          items: const [
-            Icon(Icons.keyboard_voice_rounded)
-          ],
-          color: mainThemeColor,
-          backgroundColor: Colors.white,
-          height: 60,
-        ),
-        appBar: AppBar(
-          backgroundColor: mainThemeColor,
-          title: const Text(
-            "Navigation",
-            style: TextStyle(
-                color: Colors.black,
-                fontFamily: 'Poppins'
-            ),
+      backgroundColor: const Color(0xffF2FEFE),
+      bottomNavigationBar: CurvedNavigationBar(
+        items: const [
+          Icon(Icons.keyboard_voice_rounded)
+        ],
+        color: mainThemeColor,
+        backgroundColor: Colors.white,
+        height: 60,
+      ),
+      appBar: AppBar(
+        backgroundColor: mainThemeColor,
+        title: const Text(
+          "Navigation",
+          style: TextStyle(
+              color: Colors.black,
+              fontFamily: 'Poppins'
           ),
         ),
+      ),
       body: Stack(
         children: [
           if (currentPosition != null)
@@ -136,11 +348,12 @@ class _LocationMapState extends State<LocationMap> {
                             predictions = [];
                             _destinationController.clear();
                           });
-                          }, icon: const Icon(Icons.clear_outlined),
+                        }, icon: const Icon(Icons.clear_outlined),
                       )
                           :null),
-                  onChanged: (value) {
+                    onChanged: (value) {
                     if (value.isNotEmpty) {
+                      print(value);
                       autoCompleteSearch(value);
                     } else {
                       setState(() {
@@ -168,6 +381,7 @@ class _LocationMapState extends State<LocationMap> {
                               endPosition = details.result;
                               _destinationController.text = details.result!.name!;
                               predictions = [];
+                              _stopListening();
                             });
                           }
                         },
@@ -192,9 +406,7 @@ class _LocationMapState extends State<LocationMap> {
                   ),
                   child: TextButton(
                     onPressed: () {
-                      _requestLocationPermission();
-                      _setDestination();
-                      _getPolyline();
+                      _enterDestination();
                     },
                     child: Text('Start', style: TextStyle(color: Colors.white)),
                   ),
@@ -203,7 +415,22 @@ class _LocationMapState extends State<LocationMap> {
               ],
             ),
           ),
-          Positioned(
+          Center(
+            child: ElevatedButton(
+              child: Text("CLICKME"),
+              onPressed: () async {
+                await readPredictions();
+                await _selectLocation();
+                await _setData();
+                if (_navReady){
+                  _requestLocationPermission();
+                  _setDestination();
+                  _getPolyline();
+                }
+              },
+            ),
+          ),
+          /*Positioned(
             left: 5,
             top: MediaQuery.sizeOf(context).height / 4,
             child: Column(
@@ -211,27 +438,111 @@ class _LocationMapState extends State<LocationMap> {
                 Container(
                     width: MediaQuery.sizeOf(context).width/3,
                     height: MediaQuery.sizeOf(context).height/3,
-                    child: CameraView()),
+                    child: CameraView()
+                    ),
                 Container(
                     width: 100,
                     height: 100,
-                    child: Obx(() => Text(Get.find<ScanController>().detectionResult.value))),
+                    child: Obx(() => Text(Get.find<ScanController>().detectionResult.value))
+                    ),
               ],
             ),
-          )
+          )*/
         ],
       ),
     );
   }
 
   void autoCompleteSearch(String value) async{
-    var result = await googlePlace.autocomplete.get(value);
+    var result = await googlePlace.autocomplete.get(
+        value,
+        region: "LK",
+    );
     if(result != null && result.predictions != null && mounted){
-      print(result.predictions!.first.description);
       setState(() {
         predictions = result.predictions!;
       });
     }
+  }
+
+  Future<void> readPredictions() async {
+    for (int i = 0; i < predictions.length; i++) {
+      int x = i + 1;
+      String num = x.toString();
+      await _speak("0"+num);
+      _flutterTts.awaitSpeakCompletion(true);
+      String placePredictions = predictions[i].description.toString();
+      await _speak(placePredictions);
+      _flutterTts.awaitSpeakCompletion(true);
+      print(placePredictions);
+    }
+    _isSpeaking = false;
+  }
+
+  Future<void> _selectLocation() async {
+    print("_selectLocation");
+    print(_isListening);
+    print(_isSpeaking);
+    if (!_isSpeaking && !_isListening) {
+      await _speak("Select a number");
+      print("break 1");
+      print(_isListening);
+      print(_isSpeaking);
+      if (!_isSpeaking) {
+        _isListening = true;
+        _numValue = '';
+        await _numListener();
+        await Future.delayed(Duration(seconds: 3));
+        print(_numberController.text);
+        print(_isListening);
+        print(_isSpeaking);
+      }
+    }
+    print("_selectLocation");
+  }
+
+  _setData() async {
+    print("_setData");
+    print(_isListening);
+    print(_isSpeaking);
+    if(!_isListening){
+      await _speak(_numberController.text); //numberCController,text
+      print("your number is "+ _numberController.text);
+    }
+    int? num = _getNumValue();
+    print(num);
+    try{
+      if (num!<0 || num>= predictions.length) {
+        _navReady = false;
+        print("BREAK 1");
+        _enterDestination();
+        return;
+      }
+      print("BREAK 2");
+      final placeId = predictions[num].placeId!;
+      final details = await googlePlace.details.get(placeId);
+      print(details);
+      print(details?.result);
+      if (details != null && details.result != null) {
+        print("BREAK 3");
+        setState(() {
+          endPosition = details.result;
+          _destinationController.text = details.result!.name!;
+          print(_numberController.text + _destinationController.text);
+          _speak(_numberController.text+_destinationController.text);
+          predictions = [];
+          _navReady = true;
+        });
+      } else {
+        _navReady = false;
+        _enterDestination();
+      }
+    }
+    catch (e){
+      _navReady = false;
+      _enterDestination();
+    }
+    print("_setData");
   }
 
   void _onMapCreated(GoogleMapController controller){
@@ -265,6 +576,17 @@ class _LocationMapState extends State<LocationMap> {
     final status = await Permission.locationWhenInUse.request();
     if (status == PermissionStatus.granted) {
       _getCurrentLocation();
+    } else if (status == PermissionStatus.permanentlyDenied) {
+      openAppSettings();
+    } else {
+      print('error');
+    }
+  }
+
+  Future<void> _requestMicrophoneAcess() async {
+    final status = await Permission.microphone.request();
+    if (status == PermissionStatus.granted) {
+      return;
     } else if (status == PermissionStatus.permanentlyDenied) {
       openAppSettings();
     } else {
@@ -348,16 +670,20 @@ class _LocationMapState extends State<LocationMap> {
       });
     }
     _addPolyLine();
-    Navigator.push(
-      context,
-      MaterialPageRoute(builder: (context) => TurnByTurnScreen(
-        startLatitude: s1!,
-        startLongitude: s2!,
-        endLatitude: endPosition!.geometry!.location!.lat!,
-        endLongitude: endPosition!.geometry!.location!.lng!,
-      )),
+    Future.delayed(Duration(milliseconds: delayInMilliseconds), ()
+    {
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) =>
+            TurnByTurnScreen(
+              startLatitude: s1!,
+              startLongitude: s2!,
+              endLatitude: endPosition!.geometry!.location!.lat!,
+              endLongitude: endPosition!.geometry!.location!.lng!,
+            ),
+        ),
+      );
+      },
     );
-
   }
-
 }
--- lib/screens/turnbyturn.dart
+++ lib/screens/turnbyturn.dart
@@ -6,19 +6,62 @@ import 'package:flutter/rendering.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_mapbox_navigation/flutter_mapbox_navigation.dart';
 
+import 'package:alan_voice/alan_voice.dart';
+
 void main(){
-  runApp(SampleNavigationApp());
+  runApp(
+      TurnByTurnScreen(startLatitude: 6.8, startLongitude: 79.8, endLatitude: 6.9, endLongitude: 79.9)
+  );
 }
 
+class TurnByTurnScreen  extends StatefulWidget {
+  final double startLatitude;
+  final double startLongitude;
+  final double endLatitude;
+  final double endLongitude;
 
-class SampleNavigationApp extends StatefulWidget {
+  const TurnByTurnScreen({
+    Key? key,
+    required this.startLatitude,
+    required this.startLongitude,
+    required this.endLatitude,
+    required this.endLongitude,
+  }) : super(key: key);
+
   @override
-  State<SampleNavigationApp> createState() => _SampleNavigationAppState();
+  State<TurnByTurnScreen> createState() => _TurnByTurnScreen();
 }
 
-class _SampleNavigationAppState extends State<SampleNavigationApp> {
-  final _origin = WayPoint(name: "start", latitude: 6.8, longitude: 79.8);
-  final _destination = WayPoint(name: "end", latitude: 6.9, longitude: 79.9);
+class _TurnByTurnScreen extends State<TurnByTurnScreen> {
+  _TurnByTurnScreen(){
+    AlanVoice.addButton(
+      "689e5df914105717e09b84ae8ac4018d2e956eca572e1d8b807a3e2338fdd0dc/stage",
+      buttonAlign: AlanVoice.BUTTON_ALIGN_RIGHT,
+    );
+    AlanVoice.activate();
+    AlanVoice.onCommand.add((command) => _handleCommand(command.data));
+  }
+
+  void _handleCommand(Map<String, dynamic> command) {
+    switch(command["command"]) {
+      case "build":
+        loadNav(_controller!);
+        break;
+      case "start":
+        startNav(_controller!);
+        break;
+      case "stop":
+        stopNav(_controller!);
+        break;
+      case "exit":
+        exitNav(_controller!);
+        break;
+      default:
+        debugPrint("Unknown command");
+    }
+  }
+
+  late WayPoint _origin, _destination;
 
   String? _platformVersion;
   String? _instruction;
@@ -41,16 +84,25 @@ class _SampleNavigationAppState extends State<SampleNavigationApp> {
     super.dispose();
   }
 
+  void setOptions() {
+    _navigationOption.bannerInstructionsEnabled = true;
+    _navigationOption.language = "en";
+    _navigationOption.mode = MapBoxNavigationMode.walking;
+    _navigationOption.simulateRoute = false;
+    _navigationOption.voiceInstructionsEnabled = true;
+    _navigationOption.units = VoiceUnits.imperial;
+  }
+
+  void clearOptions() {
+    _navigationOption.bannerInstructionsEnabled = false;
+    _navigationOption.voiceInstructionsEnabled = false;
+  }
+
   Future<void> initialize() async {
     if (!mounted) return;
-
     _navigationOption = MapBoxNavigation.instance.getDefaultOptions();
-    _navigationOption.language = "en";
-
     MapBoxNavigation.instance.registerRouteEventListener(_onEmbeddedRouteEvent);
-
     String? platformVersion;
-
     try {
       platformVersion = await MapBoxNavigation.instance.getPlatformVersion();
     } on PlatformException {
@@ -61,77 +113,131 @@ class _SampleNavigationAppState extends State<SampleNavigationApp> {
     });
   }
 
+  void beginNav() {
+    _origin = WayPoint(name: "start", latitude: widget.startLatitude, longitude: widget.startLongitude);
+    _destination = WayPoint(name: "end", latitude: widget.endLatitude, longitude: widget.endLongitude);
+  }
+  void loadNav(MapBoxNavigationViewController control){
+    _controller?.initialize();
+      if (_routeBuilt) {
+        control.clearRoute();
+      } else {
+        beginNav();
+        setOptions();
+        var wayPoints = <WayPoint>[];
+        wayPoints.add(_origin);
+        wayPoints.add(_destination);
+        _routeBuilt = true;
+        control.buildRoute(
+            wayPoints: wayPoints,
+            options: _navigationOption
+        );
+      }
+  }
+  void exitNav(MapBoxNavigationViewController control){
+    clearOptions();
+    control.finishNavigation();
+    _isNavigating = false;
+    _controller?.dispose();
+    control.dispose();
+    _controller?.clearRoute();
+    Navigator.pop(context);
+  }
+
+  void startNav(MapBoxNavigationViewController control){
+    if (_routeBuilt && !_isNavigating)
+      {
+        control.startNavigation();
+        _isNavigating = true;
+      }
+  }
+
+  stopNav(MapBoxNavigationViewController control) async {
+    clearOptions();
+    control.finishNavigation();
+    _isNavigating = false;
+    await Future.delayed(Duration(seconds: 3));
+    SystemNavigator.pop();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: SizedBox(
-          child: Stack(
-            children: [
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
+        body:
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Expanded(
+              flex: 4,
+              child: Container(
+                child: MapBoxNavigationView(
+                  options: _navigationOption,
+                  onRouteEvent: _onEmbeddedRouteEvent,
+                  onCreated: (MapBoxNavigationViewController controller) async {
+                    _controller = controller;
+                    controller.initialize();
+                    },
+                ),
+              ),
+            ),
+            Expanded(
+              flex: 1,
+              child:
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Expanded(
-                    flex: 1,
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width/2,
                     child:
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      verticalDirection: VerticalDirection.down,
                       children: [
                         ElevatedButton(
-                          onPressed: _isNavigating
-                              ? null
-                              : () {
-                            if (_routeBuilt) {
-                              _controller?.clearRoute();
-                            } else {
-                              var wayPoints = <WayPoint>[];
-                              wayPoints.add(_origin);
-                              wayPoints.add(_destination);
-                              _controller?.buildRoute(
-                                  wayPoints: wayPoints,
-                                  options: _navigationOption);
-                            }
+                          onPressed: () {
+                            loadNav(_controller!);
                           },
-                          child: Text(_routeBuilt && !_isNavigating
-                              ? "Clear Route"
-                              : "Build Route"),
+                          child: Text(
+                              "Build Route"
+                          ),
                         ),
                         ElevatedButton(
-                          onPressed: _routeBuilt && !_isNavigating
-                              ? () {
-                            _controller?.startNavigation();
-                          }
-                              : null,
-                          child: const Text("Go"),
-                        ),
-                        ElevatedButton(
-                          onPressed: _isNavigating
-                              ? () {
-                            _controller?.finishNavigation();
-                          }
-                              : null,
-                          child: const Text("Stop"),
+                          onPressed: () {
+                            exitNav(_controller!);
+                          },
+                          child: const Text("Exit"),
                         ),
                       ],
                     ),
                   ),
-                  Expanded(
-                    flex: 4,
-                    child: Container(
-                      child: MapBoxNavigationView(
-                        options: _navigationOption,
-                        onRouteEvent: _onEmbeddedRouteEvent,
-                        onCreated: (MapBoxNavigationViewController controller) async {
-                          _controller = controller;
-                          controller.initialize();
-                        },
-                      ),
+                  SizedBox(
+                      width: MediaQuery.of(context).size.width/2,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      verticalDirection: VerticalDirection.down,
+                      children: [
+                        ElevatedButton(
+                          onPressed: () {
+                            startNav(_controller!);
+                          },
+                          child: const Text("Go"),
+                        ),
+                        ElevatedButton(
+                          onPressed: () {
+                            stopNav(_controller!);
+                          },
+                          child: const Text("Stop"),
+                        ),
+                      ],
                     ),
-                  ),
+                  )
                 ],
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
